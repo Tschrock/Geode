c491c76242a30ad16cb21eff2909f6cb7c922560
Tyler Schrock <tschrock123@gmail.com>
2020-04-04 02:00:20 -0400

    Merge ViewComponent models

--- Geode.Web/Blocks/Cms/HtmlContentDetail.cs
+++ Geode.Web/Blocks/Cms/HtmlContentDetail.cs
@@ -15,33 +15,18 @@ namespace Geode.Web.Blocks.Cms
     public class HtmlContentDetail : GeodeWebBlock
     {
         /// <summary>
+        /// Gets or sets the content to display.
+        /// </summary>
+        public string HtmlContent { get; set; } = "<p>Hello World</p>";
+
+        /// <summary>
         /// Renders the web block.
         /// </summary>
         /// <returns>A Task containing the result of rendering the web block.</returns>
         public override async Task<IViewComponentResult> Render()
         {
             Console.WriteLine(this.Block.Name);
-            return this.View("/Blocks/Cms/HtmlContentDetail.cshtml", new Model
-            {
-                Title = this.Block.Name,
-                Content = "<p>Hello World</p>",
-            });
-        }
-
-        /// <summary>
-        /// The model for rendering the <see cref="HtmlContentDetail"/>.
-        /// </summary>
-        internal class Model
-        {
-            /// <summary>
-            /// Gets or sets the title.
-            /// </summary>
-            public string Title { get; set; } = string.Empty;
-
-            /// <summary>
-            /// Gets or sets the content.
-            /// </summary>
-            public string Content { get; set; } = string.Empty;
+            return this.View("/Blocks/Cms/HtmlContentDetail.cshtml", this);
         }
     }
 }
--- Geode.Web/Components/Zone/Zone.cshtml.cs
+++ Geode.Web/Components/Zone/Zone.cshtml.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -30,41 +31,25 @@ namespace Geode.ViewComponents
         }
 
         /// <summary>
+        /// Gets or sets the name of the zone.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the blocks in the zone.
+        /// </summary>
+        public IEnumerable<Block> Blocks { get; set; } = Array.Empty<Block>();
+
+        /// <summary>
         /// Renders the zone content.
         /// </summary>
         /// <param name="name">the name of the zone.</param>
         /// <returns>The zone content.</returns>
         public async Task<IViewComponentResult> InvokeAsync(string name)
         {
-            var blocks = this.context.Blocks.Where(b => b.Zone == name);
-            return this.View("/Components/Zone/Zone.cshtml", new Model(name, blocks));
-        }
-
-        /// <summary>
-        /// The model for rendering the zone.
-        /// </summary>
-        internal class Model
-        {
-            /// <summary>
-            /// Initializes a new instance of the <see cref="Model"/> class.
-            /// </summary>
-            /// <param name="name">The zone name.</param>
-            /// <param name="blocks">The zone blocks.</param>
-            public Model(string name, IEnumerable<Block> blocks)
-            {
-                this.Name = name;
-                this.Blocks = blocks;
-            }
-
-            /// <summary>
-            /// Gets or sets the name of the zone.
-            /// </summary>
-            public string Name { get; set; }
-
-            /// <summary>
-            /// Gets or sets the blocks in the zone.
-            /// </summary>
-            public IEnumerable<Block> Blocks { get; set; }
+            this.Blocks = this.context.Blocks.Where(b => b.Zone == name);
+            this.Name = name;
+            return this.View("/Components/Zone/Zone.cshtml", this);
         }
     }
 }
