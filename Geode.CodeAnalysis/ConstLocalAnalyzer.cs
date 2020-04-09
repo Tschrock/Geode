7684ecbd3ae7b9e8fed63487540f97a7f529cd1f
Tyler Schrock <tschrock123@gmail.com>
2020-04-09 02:30:47 -0400

    Start work on custom code analyzers

---
+++ Geode.CodeAnalysis/ConstLocalAnalyzer.cs
@@ -0,0 +1,130 @@
+// <copyright>
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Geode.CodeAnalysis
+{
+    /// <summary>
+    /// An analyzer that checks for local variables that can be made constant.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ConstLocalAnalyzer : DiagnosticAnalyzer
+    {
+        /// <summary>
+        /// The Id of the Diagnostic.
+        /// </summary>
+        public const string DiagnosticId = "G0001";
+        private const string Title = "Local variable can be made constant.";
+        private const string MessageFormat = "Local variable {0} can be made constant";
+        private const string Description = "The local variable is never written to and can be made constant.";
+        private const string Category = "Usage";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(this.AnalyzeNode, SyntaxKind.LocalDeclarationStatement);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+
+            // does not have const
+            if (localDeclaration.Modifiers.Any(SyntaxKind.ConstKeyword))
+            {
+                return;
+            }
+
+            var variableTypeName = localDeclaration.Declaration.Type;
+            var variableType = context.SemanticModel.GetTypeInfo(variableTypeName).ConvertedType;
+            if (variableType == null)
+            {
+                return;
+            }
+
+            // Ensure that all variables in the local declaration have initializers that
+            // are assigned with constant values.
+            foreach (var variable in localDeclaration.Declaration.Variables)
+            {
+                var initializer = variable.Initializer;
+                if (initializer == null)
+                {
+                    return;
+                }
+
+                var constantValue = context.SemanticModel.GetConstantValue(initializer.Value);
+                if (!constantValue.HasValue)
+                {
+                    return;
+                }
+
+                // Ensure that the initializer value can be converted to the type of the
+                // local declaration without a user-defined conversion.
+                var conversion = context.SemanticModel.ClassifyConversion(initializer.Value, variableType);
+                if (!conversion.Exists || conversion.IsUserDefined)
+                {
+                    return;
+                }
+
+                // Special cases:
+                //  * If the constant value is a string, the type of the local declaration
+                //    must be System.String.
+                //  * If the constant value is null, the type of the local declaration must
+                //    be a reference type.
+                if (constantValue.Value is string)
+                {
+                    if (variableType.SpecialType != SpecialType.System_String)
+                    {
+                        return;
+                    }
+                }
+                else if (variableType.IsReferenceType && constantValue.Value != null)
+                {
+                    return;
+                }
+            }
+
+            // Perform data flow analysis on the local declaration.
+            var dataFlowAnalysis = context.SemanticModel.AnalyzeDataFlow(localDeclaration);
+            if (dataFlowAnalysis == null)
+            {
+                return;
+            }
+
+            foreach (var variable in localDeclaration.Declaration.Variables)
+            {
+                // Retrieve the local symbol for each variable in the local declaration
+                // and ensure that it is not written outside of the data flow analysis region.
+                var variableSymbol = context.SemanticModel.GetDeclaredSymbol(variable);
+                if (variableSymbol == null || dataFlowAnalysis.WrittenOutside.Contains(variableSymbol))
+                {
+                    return;
+                }
+            }
+
+            var variableNameString = string.Join(", ", localDeclaration.Declaration.Variables.Select(v => v.Identifier.ValueText));
+            context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation(), variableNameString));
+        }
+    }
+}
