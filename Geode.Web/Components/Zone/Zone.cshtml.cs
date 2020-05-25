f9c21f1d6601e10f7c44b0cbf4da6b0475d04a87
Tschrock <tschrock123@gmail.com>
2020-05-25 19:13:54 -0400

    Add LICENSE.md and update file header

--- Geode.Web/Components/Zone/Zone.cshtml.cs
+++ Geode.Web/Components/Zone/Zone.cshtml.cs
@@ -1,5 +1,5 @@
 // <copyright>
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for full license information.
 // </copyright>
 
 using System;
